f88a45b92502cbce0a5ccec480ddf2a6f4387702
BlueArduino20 <bluearduino20@gmail.com>
2017-09-03 17:16:06 +0200

    Macchanger and safe exit added

--- APs_Beacon.sh
+++ APs_Beacon.sh
@@ -1,10 +1,23 @@
+function coolexit()
+{
+clear
+ifconfig $AD down
+macchanger -p $AD
+iwconfig $AD mode managed
+ifconfig $AD up
+clear
+echo -e "\e[1;31mThanks for using this script"
+echo -e "Made by BlueArduino20"
+exit
+}
+
 echo -e "\e[1;32m     #    ######            ######                                     "
-echo -e "\e[1;32m    # #   #     #  ####     #     # ######   ##    ####   ####  #    # "
-echo -e "\e[1;32m   #   #  #     # #         #     # #       #  #  #    # #    # ##   # "
-echo -e "\e[1;32m  #     # ######   ####     ######  #####  #    # #      #    # # #  # "
-echo -e "\e[1;32m  ####### #            #    #     # #      ###### #      #    # #  # # "
-echo -e "\e[1;32m  #     # #       #    #    #     # #      #    # #    # #    # #   ## "
-echo -e "\e[1;32m  #     # #        ####     ######  ###### #    #  ####   ####  #    # "
+echo -e "    # #   #     #  ####     #     # ######   ##    ####   ####  #    # "
+echo -e "   #   #  #     # #         #     # #       #  #  #    # #    # ##   # "
+echo -e "  #     # ######   ####     ######  #####  #    # #      #    # # #  # "
+echo -e "  ####### #            #    #     # #      ###### #      #    # #  # # "
+echo -e "  #     # #       #    #    #     # #      #    # #    # #    # #   ## "
+echo -e "  #     # #        ####     ######  ###### #    #  ####   ####  #    # "
 echo " "
 echo -e "\e[1;37m                                                     By BlueArduino20  "
 echo " "
@@ -12,26 +25,33 @@ ifconfig
 echo -n -e "\e[1;34mType your wireless adapter name > "
 read AD
 clear
-echo -n -e "\e[1;34mDo you want to use the default dictionary (1000 APs)? y/n > "
+echo -n -e "Do you want to use the default dictionary (1000 APs)? y/n > "
 read DD
 clear
 if [ $DD == n ]; then
-	echo -n -e "\e[1;34mType the name of your own dictionary > "
+	echo -n -e "Type the name of your own dictionary > "
 	read OWN
 	clear
-	echo -e "\e[1;34mOk. If you want to stop the process, press CTRL+Z"
+	echo -e "Ok. If you want to stop the process, press CTRL+C"
+	echo -e "\e[37m"
+	trap coolexit EXIT
 	sleep 1
 	ifconfig $AD down
+	macchanger -r $AD
 	iwconfig $AD mode monitor
 	ifconfig $AD up
 	mdk3 $AD b -f ./$OWN -a -s 1000
-	exit
 else
 	clear
-	echo -e "\e[1;34mOk. If you want to stop the process, press CTRL+Z"
+	echo -e "Ok. If you want to stop the process, press CTRL+C"
+	echo -e "\e[37m"
+	trap coolexit EXIT
 	sleep 1
 	ifconfig $AD down
+	macchanger -r $AD
 	iwconfig $AD mode monitor
 	ifconfig $AD up
 	mdk3 $AD b -f ./SSID_List.txt -a -s 1000
+	trap exit EXIT
 fi
+
