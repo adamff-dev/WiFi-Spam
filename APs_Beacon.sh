41750a71134d94461d8a7d8461f115534f542ec7
BlueArduino20 <bluearduino20@gmail.com>
2018-02-06 21:03:56 +0100

    WiFi Spam (APs_Beacon 2.0): More options & cooler
    New options:
    -Create a fast custom word list and use it
    -Use an existent word list
    -Use a random SSIDs word list

--- APs_Beacon.sh
+++ APs_Beacon.sh
@@ -1,57 +1,132 @@
-function coolexit()
-{
+#!/bin/bash
+
+function coolexit() {
 clear
-ifconfig $AD down
-macchanger -p $AD
-iwconfig $AD mode managed
-ifconfig $AD up
+>nul ifconfig $AD down
+>nul macchanger -p $AD
+>nul iwconfig $AD mode managed
+>nul ifconfig $AD up
+>nul rm RANDOM_wordlist.txt
 clear
-echo -e "\e[1;31mThanks for using this script"
-echo -e "Made by BlueArduino20"
+title
+echo -e "\e[1;31m   Thanks for using this script"
+echo -e "   My GitHub: \e[1;37mhttps://github.com/BlueArduino20"
 exit
 }
 
-echo -e "\e[1;32m     #    ######            ######                                     "
-echo -e "    # #   #     #  ####     #     # ######   ##    ####   ####  #    # "
-echo -e "   #   #  #     # #         #     # #       #  #  #    # #    # ##   # "
-echo -e "  #     # ######   ####     ######  #####  #    # #      #    # # #  # "
-echo -e "  ####### #            #    #     # #      ###### #      #    # #  # # "
-echo -e "  #     # #       #    #    #     # #      #    # #    # #    # #   ## "
-echo -e "  #     # #        ####     ######  ###### #    #  ####   ####  #    # "
+function title() {
+echo -e "\e[1;32m    _       ___ _______    _____                     "
+echo -e "   | |     / (_) ____(_)  / ___/____  ____ _____ ___ "
+echo -e "   | | /| / / / /_  / /   \\__ \\/ __ \\/ __ \`/ __ \`__ \\"
+echo -e "   | |/ |/ / / __/ / /   ___/ / /_/ / /_/ / / / / / /"
+echo -e "   |__/|__/_/_/   /_/   /____/ .___/\\__,_/_/ /_/ /_/"
+echo -e "                            /_/                      "
+echo -e "\e[1;37m                                     By BlueArduino20  "
 echo " "
-echo -e "\e[1;37m                                                     By BlueArduino20  "
-echo " "
+}
+
+title
+sleep 1
 ifconfig
 echo -n -e "\e[1;34mType your wireless adapter name > "
+echo -e -n "\e[1;37m"
 read AD
 clear
-echo -n -e "Do you want to use the default dictionary (1000 APs)? y/n > "
+title
+echo -e "\e[1;34m  Choose an option:"
+echo " "
+echo -e "\e[1;34m  1. \e[1;37mUse the default word list (1000 APs)"
+echo -e "\e[1;34m  2. \e[1;37mCreate a fast custom word list and use it"
+echo -e "\e[1;34m  3. \e[1;37mUse an existent word list"
+echo -e "\e[1;34m  4. \e[1;37mUse a random SSIDs word list"
+echo " "
+echo -n -e "\e[1;34m  > "
+echo -n -e "\e[1;37m"
 read DD
 clear
-if [ $DD == n ]; then
-	echo -n -e "Type the name of your own dictionary > "
+if [ $DD == 1 ]; then
+	clear
+	title
+	echo -e "\e[1;32mStarting process..."
+	echo -e "If you want to stop it press CTRL+C."
+	echo -e "\e[37m "
+	trap coolexit EXIT
+	sleep 1
+	ifconfig $AD down
+	macchanger -r $AD
+	iwconfig $AD mode monitor
+	ifconfig $AD up
+	trap coolexit EXIT
+	mdk3 $AD b -f ./SSID_List.txt -a -s 1000
+fi
+if [ $DD == 2 ]; then
+	clear
+	title
+	echo -n -e "\e[1;34mWrite your string/word (Max. length: 12) > "
+	echo -n -e "\e[1;37m"
+	read WORD
+	echo -n -e "\e[1;34mHow many SSIDs do you want? > "
+	echo -n -e "\e[1;37m"
+	read N
+	COUNT=1
+	while [ $COUNT -lt $N ] || [ $COUNT -eq $N ]; do
+		echo $WORD $COUNT >> $WORD"_wordlist.txt"
+		let COUNT=COUNT+1
+	done
+	clear
+	title
+	echo -e "\e[1;32mStarting process..."
+	echo -e "If you want to stop it press CTRL+C."
+	echo -e "\e[37m"
+	trap coolexit EXIT
+	sleep 1
+	ifconfig $AD down
+	macchanger -r $AD
+	iwconfig $AD mode monitor
+	ifconfig $AD up
+	trap coolexit EXIT
+	mdk3 $AD b -f ./$WORD"_wordlist.txt" -a -s 1000
+fi
+if [ $DD == 3 ]; then
+	title
+	echo -e "\e[1;37mPlease, note that your word list must have the same structure as SSID_List.txt, otherwise the process won't work."
+	echo " "
+	echo -n -e "\e[1;34mType the name of your own word list > "
+	echo -n -e "\e[1;37m"
 	read OWN
 	clear
-	echo -e "Ok. If you want to stop the process, press CTRL+C"
-	echo -e "\e[37m"
-	trap coolexit EXIT
+	title
+	echo -e "\e[1;32mStarting process..."
+	echo -e "If you want to stop it press CTRL+C."
+	echo -e "\e[1;37m"
 	sleep 1
 	ifconfig $AD down
 	macchanger -r $AD
 	iwconfig $AD mode monitor
 	ifconfig $AD up
-	mdk3 $AD b -f ./$OWN -a -s 1000
-else
-	clear
-	echo -e "Ok. If you want to stop the process, press CTRL+C"
-	echo -e "\e[37m"
 	trap coolexit EXIT
-	sleep 1
-	ifconfig $AD down
-	macchanger -r $AD
-	iwconfig $AD mode monitor
-	ifconfig $AD up
-	mdk3 $AD b -f ./SSID_List.txt -a -s 1000
-	trap exit EXIT
+	mdk3 $AD b -f ./$OWN -a -s $(wc -l $OWN | cut -f1 -d ' ')
 fi
-
+if [ $DD == 4 ]; then
+	title
+	echo -n -e "\e[1;34mHow many SSIDs do you want? > "
+	echo -n -e "\e[1;37m"
+	read N
+	COUNT=1
+	while [ $COUNT -lt $N ] || [ $COUNT -eq $N ]; do
+		echo $(pwgen 14 1) >> "RANDOM_wordlist.txt"
+		let COUNT=COUNT+1
+	done
+	clear
+	title
+	echo -e "\e[1;32mStarting process..."
+	echo -e "If you want to stop it press CTRL+C."
+	echo -e "\e[1;37m "
+	sleep 1
+	ifconfig $AD down
+	macchanger -r $AD
+	iwconfig $AD mode monitor
+	ifconfig $AD up
+	trap coolexit EXIT
+	mdk3 $AD b -f ./RANDOM_wordlist.txt -a -s $N
+fi
