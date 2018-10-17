2db52ae352820a1082d039fab4666980c4a9c38f
BlueArduino20 <bluearduino20@gmail.com>
2018-10-17 22:59:11 +0200

    pwgen added

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ apt install ./kali-archive-keyring_2018.1_all.deb
 echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" >> /etc/apt/sources.list
 echo "deb http://repo.kali.org/kali kali-bleeding-edge main' >> /etc/apt/sources.list"
 apt-get update  
-apt-get install -y mdk3 macchanger
+apt-get install -y mdk3 macchanger pwgen
 echo "Done!"
